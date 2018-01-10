a2fb1bda88aa5fb520e5be729009ac553a1ce6b0
chenfeng1992 <592585244@qq.com>
2018-01-10 12:27:36 +0800

    django
    一个简单的djang小例子

---
+++ myblog/blog/models.py
@@ -0,0 +1,11 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Article(models.Model):
+    title = models.CharField(max_length=32,default='Title')
+    content = models.TextField(null=True)
+    pub_time = models.DateTimeField(auto_now=True,null=True)
+
+    def __unicode__(self):
+        return self.title
